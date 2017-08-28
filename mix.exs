82974b3b9a0c71eb65a6b20deef2178c00292b05
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-08-28 23:03:37 +0300

    Version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule CryptopiaApi.Mixfile do
   def project do
     [
       app: :cryptopia_api,
-      version: "0.1.0",
+      version: "0.2.0",
       elixir: "~> 1.3",
       description: "Elixir wrapped cryptopia API",
       deps: deps(),
